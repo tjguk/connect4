b476c09eb531fd1b7c49288ac7231b0eafeb26c8
Tim Golden <mail@timgolden.me.uk>
2011-09-30 18:33:42 +0100

    All unittests working for the Board object

--- connect4.py
+++ connect4.py
@@ -1,11 +1,15 @@
 import os, sys
 import itertools
+import logging
+
+log = logging.getLogger ("connect4")
 
 class Board (object):
 
-  def __init__ (self, n_columns=7, n_rows=6, board=None):
+  def __init__ (self, n_columns=7, n_rows=6, n_to_win=4, board=None):
     self.columns = range (1, n_columns+1)
     self.rows = range (1, n_rows+1)
+    self.n_to_win = n_to_win
     if board:
       self._board = dict (board)
     else:
@@ -17,28 +21,57 @@ class Board (object):
   def __str__ (self):
     return self.as_string ()
 
+  def __getitem__ (self, item):
+    return self._board[item]
+
   def as_string (self):
     return "\n".join ("|".join (self._board.get ((column, row), ".") for column in self.columns) for row in self.rows[::-1])
 
+  def counters (self):
+    return dict (self._board)
+
+  def column_height (self, column):
+    return max ([r for c, r in self._board.keys () if c == column] or [0])
+
+  def valid_columns (self):
+    """Determine all valid columns from the board's current position. This can
+    be used both to validate an entered move and to look ahead
+    """
+    for column in self.columns:
+      row = 1 + self.column_height (column)
+      if row <= max (self.rows):
+        yield column
+
+  def make_move (self, player, column):
+    """Make a player's move by "dropping" his counter onto the highest available
+    position in a column.
+    """
+    self._board[column, 1 + self.column_height (column)] = player
+
   def intersecting_lines (self, column, row):
+    """Determine which lines (horizontal, vertical or diagonal) intersect
+    a position on the board -- typically one which has just been played.
+    """
     yield [(c, r) for c in self.columns for r in self.rows if c == column]
     yield [(c, r) for c in self.columns for r in self.rows if r == row]
     yield [(c, r) for c in self.columns for r in self.rows if c + r == column + row]
     yield [(c, r) for c in self.columns for r in self.rows if c - r == column - row]
 
-  def column_height (self, column):
-    return max ([r for c, r in self._board.keys () if c == column] or [0])
+  def win_for (self, player, column, row):
+    """Determine whether a specific player has won. Given the nature of the game,
+    only the player who has just moved can possibly have won, and only by completing
+    a line which contains his latest position.
+    """
+    winning_run = player * self.n_to_win
+    log.debug ("winning_run = %s", winning_run)
+    for line in self.intersecting_lines (column, row):
+      log.debug ("line: %s", "".join (self._board.get ((c, r), ".") for (c, r) in line))
+      if winning_run in "".join (self._board.get ((c, r), ".") for (c, r) in line):
+        return True
+    else:
+      return False
 
-  def possible_moves (self):
-    for column in self.columns:
-      row = 1 + self.column_height (column)
-      if row <= max (self.rows):
-        yield column, row
-
-  def make_move (self, player, column):
-    self._board[column, 1 + self.column_height (column)] = player
-
-  def win_for (self, n_to_win=4):
+  def DO_NOT_USE (self):
     for player in set (self._board.values ()):
       winning_run = player * n_to_win
 
@@ -59,13 +92,16 @@ class Board (object):
 
     return None
 
-  def project (self, player, counter):
+  def project (self, player, column):
+    """Return the state of the board after a player has dropped their counter
+    in a particular column. This would usually be used to look ahead.
+    """
     board = self.__class__ (
       n_columns=len (self.columns),
       n_rows=len(self.rows),
       board=self._board
     )
-    board.make_move (player, counter)
+    board.make_move (player, column)
     return board
 
 class Game (object):
@@ -78,9 +114,10 @@ class Game (object):
 
   def turn (self, player, column):
     print "Turn for player %r - %r" % (player, column)
-    if column not in (c for c, r in self.board.possible_moves ()):
+    valid_columns = list (self.board.valid_columns ())
+    if column not in valid_columns:
       raise ValueError, "Invalid move"
-    self.board.make_move (column, player)
+    self.board.make_move (player, column)
 
   def play (self):
     print self.board.as_string ()
--- test_connect4.py
+++ test_connect4.py
@@ -1,3 +1,71 @@
 import os, sys
-
+import logging
 import unittest
+
+import connect4
+#~ connect4.log.setLevel (logging.DEBUG)
+connect4.log.addHandler (logging.StreamHandler ())
+
+class TestBoard (unittest.TestCase):
+
+  def setUp (self):
+    self.board = connect4.Board ()
+
+  def tearDown (self):
+    pass
+
+  def test_empty_board (self):
+    board = connect4.Board ()
+    self.assertFalse (board.counters ())
+
+  def test_board_rows (self):
+    board = connect4.Board (n_rows=3)
+    self.assertEqual (board.rows, [1, 2, 3])
+
+  def test_board_columns (self):
+    board = connect4.Board (n_columns=3)
+    self.assertEqual (board.columns, [1, 2, 3])
+
+  def test_board_to_win (self):
+    board = connect4.Board (n_to_win=3)
+    self.assertFalse (board.win_for ('X', 1, 1))
+    board.make_move ('X', 1)
+    self.assertFalse (board.win_for ('X', 1, 1))
+    board.make_move ('X', 1)
+    self.assertFalse (board.win_for ('X', 1, 1))
+    board.make_move ('X', 1)
+    self.assertTrue (board.win_for ('X', 1, 1))
+
+  def test_valid_columns (self):
+    self.assertEqual (list (self.board.valid_columns ()), self.board.columns)
+
+  def test_column_height (self):
+    self.assertEqual (self.board.column_height (1), 0)
+    self.board.make_move ('X', 1)
+    self.assertEqual (self.board.column_height (1), 1)
+
+  def test_make_move (self):
+    self.assertEqual (self.board.counters (), {})
+    self.board.make_move ('X', 1)
+    self.assertEqual (self.board.counters (), {(1, 1) : 'X'})
+
+  def test_intersecting_lines (self):
+    board = connect4.Board (3, 3)
+    self.assertEqual (
+      set (tuple (line) for line in board.intersecting_lines (2, 2)),
+      set ([
+        ((1, 1), (2, 2), (3, 3)),
+        ((1, 3), (2, 2), (3, 1)),
+        ((1, 2), (2, 2), (3, 2)),
+        ((2, 1), (2, 2), (2, 3))
+      ])
+    )
+
+  def test_project (self):
+    board = self.board.project ('X', 1)
+    self.assertIsNot (board, self.board)
+    self.assertNotEqual (board.counters (), self.board.counters ())
+    self.assertDictEqual (board.counters (), {(1, 1) : 'X'})
+
+if __name__ == '__main__':
+  unittest.main ()
